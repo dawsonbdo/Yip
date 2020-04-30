303f689108d881c2d0a129185220cceb2a04469d
gokular <garasmi@ucsd.edu>
2020-04-30 16:10:37 -0700

    fixed FE

--- frontend/src/js/components/YipNavBar.js
+++ frontend/src/js/components/YipNavBar.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-import { Navbar } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import FormGroup from 'react-bootstrap/FormGroup';
 
 class YipNavBar extends Component {
