c0349dc0eca45aec930cb07b8bbd412a1be1deb9
Noswadod <dodawson14@gmail.com>
2020-05-02 16:08:53 -0700

    svg webpacking

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,9 +11,13 @@ module.exports = {
 		  use: ["style-loader", "css-loader"]
     },
     {
-      test: /\.(png|svg|jpg|gif)$/,
+      test: /\.(png|jpg|gif)$/,
       use: ["url-loader"]
     },
+    {
+      test: /\.svg$/,
+      loader: 'svg-inline-loader'
+    }
     ]
   }
 };
