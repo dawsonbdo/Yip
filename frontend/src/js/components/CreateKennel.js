c1e790a947e2a70439ca54096cc45d57e25d0148
gokular <garasmi@ucsd.edu>
2020-05-13 16:56:48 -0700

    changed createkennel.js

--- frontend/src/js/components/CreateKennel.js
+++ frontend/src/js/components/CreateKennel.js
@@ -1,59 +1,123 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import corgiImage from '../../assets/corgi_shadow.png';
+import { Redirect } from 'react-router-dom';
 
-import axios from 'axios' 
+import axios from 'axios'
 
 import { createUserJson } from './BackendHelpers.js';
 
-class CreateReview extends Component {
+class CreateKennel extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: null,
+      validated: false
+    };
+
+    // Binds button handler
+    this.updateKennel = this.updateKennel.bind(this);
+  }
+
+  /**
+   * Function handler for edit kennel submit button
+   */
+  updateKennel(event) {
+
+    // Prevents page from refreshing on submit
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.setState({ failedLogin: false });
+
+    var registerForm = event.currentTarget;
+
+    // Displays error if fields are empty
+    if (registerForm.checkValidity() === false) {
+      this.setState({ validated: true });
+      return;
+    }
+
+    // Parses login form with username/email and password
+    var email = document.getElementById('login').value;
+    var username = document.getElementById('login').value;
+    var password = document.getElementById('password').value
+    var form = createUserJson(username, email, password);
+
+    // Send POST request with username, email, and password
+    axios({
+      method: 'post',
+      url: '/login',
+      data: form
+    }).then(response => {
+
+      // Store token in local storage
+      localStorage.setItem('jwtToken', response.data);
+
+      // Redirect to home after successful login
+      this.setState({ redirect: "/" });
+
+
+    }).catch(error => {
+
+      // Error for failed login
+      this.setState({ failedLogin: true });
+      alert('Username or Password incorrect!');
+
+    });
+  }
 
   render() {
-    return (
-      <Container fluid>
-        <Row className="align-items-center">
-          <Col></Col>
-          <Col className="text-center">
-            <Link to="/"><img src={corgiImage} /></Link>
-            <div className="logInForm">
-              <h1 className="logInLabel">Create Review</h1>
-              <Form className="logInEntryContainer">
-                <div className="logInEntryContainer">
-                  <Form.Control id="login" className="logInEntry" size="lg" type="text" placeholder="Title" />
-                </div>
-                <div className="logInEntryContainer">
-                  <Form.Control id="password" className="logInEntry" size="lg" as="textarea" placeholder="Enter Review Description" />
-                </div>
-                <div className="logInEntryContainer">
-                    <ButtonToolbar>
-                        <ButtonGroup className="mx-auto" aria-label="First group">
-                            <Button>Wag</Button> 
-                            <Button className="ml-2">Growl</Button> 
-                        </ButtonGroup>
-                    </ButtonToolbar>
-                </div>
-                <div>
-                  <Link><Button variant="link">Forgot Password?</Button></Link>
-                </div>
-                <div className="logInEntryContainer">
-                  <Button onClick={this.attemptLogin} className="logInEntry" variant="primary">Submit</Button>
-                </div>
-              </Form>
-            </div>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
-    )
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }
+    else {
+      return (
+        <Container>
+          <Row className="align-items-center">
+            
+            <Col className="text-center">
+              <Link to="/"><img src={corgiImage} /></Link>
+              <div className="logInForm">
+                <h1 className="logInLabel">Create Kennel</h1>
+                <Form noValidate validated={this.state.validated} onSubmit={this.updateKennel} className="logInEntryContainer">
+                  <div className="logInEntryContainer">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control id="login" className="logInEntry" type="text"/>
+                  </div>
+                  <div className="logInEntryContainer">
+                    <Form.Label>Rules</Form.Label>
+                    <Form.Control id="login" className="logInEntry" type="text" as="textarea"/>
+                  </div>
+                  <div className="logInEntryContainer">
+                    <Form.Label>Tags</Form.Label>
+                    <Form.Control id="login" className="logInEntry" type="text"/>
+                  </div>
+                  <div className="logInEntryContainer">
+                    <Form.Label>Muted Words</Form.Label>
+                    <Form.Control id="login" className="logInEntry" type="text"/>
+                  </div>
+                  <div className="logInEntryContainer">
+                    <Button className="logInEntry" variant="primary">Save</Button>
+                  </div>
+                </Form>
+              </div>
+            </Col>
+            
+          </Row>
+        </Container>
+      )
+    }
   }
+
 }
 
-export default CreateReview;
+export default CreateKennel;
