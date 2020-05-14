4c3c835462eb6a94535937a089dcc82395e4baab
Noswadod <dodawson14@gmail.com>
2020-05-14 16:59:57 -0700

    inbox shows only when logged in

--- frontend/src/js/components/YipNavBar.js
+++ frontend/src/js/components/YipNavBar.js
@@ -62,10 +62,15 @@ class YipNavBar extends Component {
   render() {
     let logBtn;
     if (isLoggedIn(this)) {
-      logBtn = <Button onClick={this.logout} type="submit" variant="warning" className="mr-5">Logout</Button>;
+      logBtn = <div>
+                <Button className="mr-5" variant="warning">Inbox</Button>
+                <Button onClick={this.logout} type="submit" variant="warning" className="mr-5">Logout</Button>
+               </div>;
     } else {
-      logBtn = <div><Link to="/login"><Button id="login" type="submit" variant="warning" className="mr-5">Login</Button></Link>
-        <Link to="/register"><Button type="submit" variant="warning" className="mr-5">Register</Button></Link></div>;
+      logBtn = <div>
+                <Link to="/login"><Button id="login" type="submit" variant="warning" className="mr-5">Login</Button></Link>
+                <Link to="/register"><Button type="submit" variant="warning" className="mr-5">Register</Button></Link>
+               </div>;
     }
     const followedKennels = this.state.followedKennelsArray.map(function (kennel) {
       return <Dropdown.Item as="button">{kennel}</Dropdown.Item>
@@ -87,7 +92,6 @@ class YipNavBar extends Component {
               <Nav.Link href="#link">Link</Nav.Link>
             </Nav>
           </Navbar.Collapse> */}
-          {/* <Button className="" variant="warning">Inbox</Button> */}
           <Form inline className="ml-auto float-right pt-3">
             <FormGroup>
               <FormControl id="searchBar" type="text" placeholder="Search for Reviews and Kennels" />
