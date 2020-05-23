764520a945bf9bfc81d05d30cfd59dd2e914ba2c
Emmeryn G <egelblic@ucsd.edu>
2020-05-23 16:57:08 -0700

    added From impls for error type

--- backend/src/error/mod.rs
+++ backend/src/error/mod.rs
@@ -1,10 +1,25 @@
-use diesel::result::Error as DieselError;
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub enum Error{
-    IO(std::io::Result<()>),
-    DieselError(DieselError),
+    IO(std::io::Error),
+    Diesel(diesel::result::Error),
+    JsonWebToken(jsonwebtoken::errors::Error)
 }
 
-impl Error{
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error{
+        Error::IO(err)
+    }
+}
 
+impl From<diesel::result::Error> for Error {
+    fn from(err: diesel::result::Error) -> Error{
+        Error::Diesel(err)
+    }
+}
+
+impl From<jsonwebtoken::errors::Error> for Error {
+    fn from(err: jsonwebtoken::errors::Error) -> Error{
+        Error::JsonWebToken(err)
+    }
 }
